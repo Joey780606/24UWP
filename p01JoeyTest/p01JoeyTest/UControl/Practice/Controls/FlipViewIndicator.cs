abcab7b30b47f72c1f980a6853a0efcc206ac113
JoeyYang <joeyyang@pcpartner.com>
2024-03-12 15:21:26 +0800

    Practice: Join Test01, Test02
    Test02 needs to join some comment later.

---
+++ p01JoeyTest/p01JoeyTest/UControl/Practice/Controls/FlipViewIndicator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls; //ListBox會用到
+using Windows.UI.Xaml.Data;  //Binding好像也是要用這個
+
+namespace p01JoeyTest.UControl.Practice.Controls
+{
+    class FlipViewIndicator:ListBox
+    {
+        public FlipViewIndicator()
+        {
+            this.DefaultStyleKey = typeof(FlipViewIndicator);   //不知為何要用這個
+        }
+
+        public FlipView FlipView
+        {
+            get { return (FlipView)GetValue(FlipViewProperty); }    //寫這行後,就有建議要建立 DependencyProperty 變數型態的 FlipViewProperty
+            set { SetValue(FlipViewProperty, value); }
+        }
+
+        // 非常重要
+        public DependencyProperty FlipViewProperty =
+            DependencyProperty.Register("FlipView", typeof(FlipView), typeof(FlipViewIndicator), new PropertyMetadata(null, (depobj, args) => {
+                FlipViewIndicator fvi = (FlipViewIndicator)depobj;
+                FlipView fv = (FlipView)args.NewValue;
+                fv.SelectionChanged += (s, e) =>
+                {
+                    fvi.ItemsSource = fv.ItemsSource;
+                };
+                fvi.ItemsSource = fv.ItemsSource;
+                Binding eb = new Binding();
+                eb.Mode = BindingMode.TwoWay;
+                eb.Source = fv;
+                eb.Path = new PropertyPath("SelectedItem");
+                fvi.SetBinding(FlipViewIndicator.SelectedItemProperty, eb);
+            }));
+    }
+}
---
+++ p01JoeyTest/p01JoeyTest/UControl/Practice/Test02.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+/*
+ * Author: Joey
+ * Reference: https://www.youtube.com/watch?v=cGfNMDWYlUs&list=PLaoF-xhnnrRUNVx-JAfEy_kUrGGaKS7HL&index=1
+ * Important:
+ *   1.Controls/FlipViewIndicator.cs 是 ListBox的 control 程式,重要.
+ */
+namespace p01JoeyTest.UControl.Practice
+{
+    public class MyItem
+    {
+        public string ImagePath { get; set; }
+        public string ImageName { get; set; }
+    }
+
+    public sealed partial class Test02 : UserControl
+    {
+        List<MyItem> listImages = new List<MyItem>();
+
+        public Test02()
+        {
+            this.InitializeComponent();
+            InitImage();
+            flipView.ItemsSource = listImages;
+        }
+
+        /*
+        public override void OnNavigatedTo(NavigationEventArgs e)   //好像只有 Page有
+        {
+            base.OnNavigatedTo(e);
+            InitImage();
+            flipView.ItemsSource = listImages;
+        }
+        */
+        private void InitImage()
+        {
+            listImages.Add(new MyItem() { ImageName = "Epic", ImagePath = "ms-appx:///Assets/ic_game_epic.png" });
+            listImages.Add(new MyItem() { ImageName = "Gog", ImagePath = "ms-appx:///Assets/ic_game_gog.png" });
+            listImages.Add(new MyItem() { ImageName = "MS", ImagePath = "ms-appx:///Assets/ic_game_ms_store.png" });
+            listImages.Add(new MyItem() { ImageName = "Ubisoft", ImagePath = "ms-appx:///Assets/ic_game_ubisoft.png" });
+            listImages.Add(new MyItem() { ImageName = "xBox", ImagePath = "ms-appx:///Assets/ic_game_xbox.png" });
+        }
+    }
+}
