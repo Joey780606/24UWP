feeed2cc5f9f9c5812b79a9d110f7baa1cf906b3
JoeyYang <joeyyang@pcpartner.com>
2024-06-19 13:16:55 +0800

    NAS file download test
    Process is OK, but result is failed.

--- p01JoeyTest/p01JoeyTest/UControl/Practice/Page01/Page01.xaml.cs
+++ p01JoeyTest/p01JoeyTest/UControl/Practice/Page01/Page01.xaml.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.UI.Controls.TextToolbarSymbols;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Radios;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking.BackgroundTransfer;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -48,6 +52,24 @@ namespace p01JoeyTest.UControl.Practice.Page01
                 if (bluetooth != null && bluetooth.State != RadioState.Off)
                     await bluetooth.SetStateAsync(RadioState.Off);
             }
+        }
+
+        /// <summary>
+        /// Test download program. Ref: https://stackoverflow.com/questions/45484802/download-pdf-and-word-file-in-uwp
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void DownloadFile_Click(object sender, RoutedEventArgs e)
+        {
+            Uri source = new Uri(@"http://192.168.0.12:8080/share.cgi?ssid=2bd05be97b6a4e87ab2f7b7bf6b55574");
+            StorageFile destinationFile = await KnownFolders.PicturesLibrary.CreateFileAsync("version.txt", CreationCollisionOption.GenerateUniqueName);
+            BackgroundDownloader downloader = new BackgroundDownloader();
+            DownloadOperation download = downloader.CreateDownload(source, destinationFile);
+            await download.StartAsync();
+            Debug.WriteLine("Download success" + ApplicationData.Current.LocalFolder.Path);
+            //存到系統的Pictures目錄裡
+            //結果失敗,有存下來,但變成圖片型式
+
         }
     }
 }
