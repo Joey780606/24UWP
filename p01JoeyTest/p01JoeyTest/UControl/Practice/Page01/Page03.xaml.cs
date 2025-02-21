a3875af875f294592cea511e40abd3d0a5e192de
JoeyYang <joeyyang@pcpartner.com>
2025-02-21 10:05:35 +0800

    Record p9 program

--- p01JoeyTest/p01JoeyTest/UControl/Practice/Page01/Page03.xaml.cs
+++ p01JoeyTest/p01JoeyTest/UControl/Practice/Page01/Page03.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -25,6 +26,33 @@ namespace p01JoeyTest.UControl.Practice.Page01
         public Page03()
         {
             this.InitializeComponent();
+        }
+
+        private void P0001_Lambda_Click(object sender, RoutedEventArgs e)
+        {
+            var p1 = new P0001_MutablePoint(1, 2);
+            var p2 = p1;
+            p2.Y = 200;
+            Debug.WriteLine($"{nameof(p1)} after {nameof(p2)} is modified:{p1}");
+            Debug.WriteLine($"{nameof(p2)}: {p2}");
+
+            P0001_MutateAndDisplay(p2);
+            Debug.WriteLine($"{nameof(p2)} after passing to a method: {p2}");
+        }
+
+        private void P0001_MutateAndDisplay(P0001_MutablePoint p)
+        {
+            p.X = 100;
+            Debug.WriteLine($"Point mutated in a method: {p}");
+        }
+
+        private struct P0001_MutablePoint
+        {
+            public int X;
+            public int Y;
+
+            public P0001_MutablePoint(int x, int y) => (X, Y) = (x, y);
+            public override string ToString() => $"({X}, {Y})";
         }
     }
 }
