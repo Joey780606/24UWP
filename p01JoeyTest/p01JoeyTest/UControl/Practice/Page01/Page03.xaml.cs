983086ea7c509aae547925f99846a732cb748681
JoeyYang <joeyyang@pcpartner.com>
2025-02-24 09:58:34 +0800

    Record p18 program

--- p01JoeyTest/p01JoeyTest/UControl/Practice/Page01/Page03.xaml.cs
+++ p01JoeyTest/p01JoeyTest/UControl/Practice/Page01/Page03.xaml.cs
@@ -46,6 +46,20 @@ namespace p01JoeyTest.UControl.Practice.Page01
             Debug.WriteLine($"Point mutated in a method: {p}");
         }
 
+        private void P0002_Lambda_Click(object sender, RoutedEventArgs e) //p10
+        {
+            var n1 = new P0002_TaggedInteger(0);
+            n1.AddTag("A");
+            Debug.WriteLine(n1);
+
+            var n2 = n1;
+            n2.Number = 7;
+            n2.AddTag("B");  //重要,List 是 reference
+
+            Debug.WriteLine(n1);
+            Debug.WriteLine(n2);
+        }
+
         private struct P0001_MutablePoint
         {
             public int X;
@@ -53,6 +67,53 @@ namespace p01JoeyTest.UControl.Practice.Page01
 
             public P0001_MutablePoint(int x, int y) => (X, Y) = (x, y);
             public override string ToString() => $"({X}, {Y})";
+        }
+
+        public struct P0002_TaggedInteger
+        {
+            public int Number;
+            private List<string> tags;
+
+            public P0002_TaggedInteger(int n)
+            {
+                Number = n;
+                tags = new List<string>();
+            }
+
+            public void AddTag(string tag) => tags.Add(tag);
+            public override string ToString() => $"{Number} [{string.Join(", ", tags)}]";
+        }
+
+        private void P0003_Variable_Click(object sender, RoutedEventArgs e) //p14
+        {
+            //下面二個相同
+            int a = 123;
+            System.Int32 b = 123;
+            var decimalLiteral = 42;    //不用前綴 prefix
+            var hexLiteral = 0x2A;  //16進位
+            var binaryLiteral = 0b_0010_1010; //1.2進制
+
+            int testValue = unchecked((int)0xFF_FF_FF_FF);
+
+            byte byteA = 17;
+            //byte byteB = 300;  //重要,會有錯,因為只有到0~255
+
+            //型態強制轉換
+            var signedByte = (sbyte)42;
+            var longVariable = (long)42;
+
+            //以下似乎要 C# 9.0才能使用
+            //unsafe
+            //{
+            //    Debug.WriteLine($"size of nint = {sizeof(nint)}");
+            //}
+
+            //Debug.WriteLine($"size of nint = {sizeof(nuint)}");
+
+            //Debug.WriteLine($"nint.MinValue = {nint.MinValue}");
+            //Debug.WriteLine($"nint.MinValue = {nint.MaxValue}");
+            //Debug.WriteLine($"nint.MinValue = {nuint.MinValue}");
+            //Debug.WriteLine($"nint.MinValue = {nuint.MaxValue}");
         }
     }
 }
